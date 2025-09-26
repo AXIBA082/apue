453d73724c6a89845a219415d41f72045805a313
gpu024 <1093898952@qq.com>
2025-09-26 15:13:37 +0800

    cmake

---
+++ build/intro/CMakeFiles/shelll.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shelll.dir/shell1.c.o"
+  "shelll"
+  "shelll.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shelll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/lib/CMakeFiles/apue.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/maorui/apue/lib/Orecvfd.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/Orecvfd.c.o"
+  "/home/maorui/apue/lib/bufargs.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/bufargs.c.o"
+  "/home/maorui/apue/lib/cliconn.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/cliconn.c.o"
+  "/home/maorui/apue/lib/clrfl.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/clrfl.c.o"
+  "/home/maorui/apue/lib/daemonize.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/daemonize.c.o"
+  "/home/maorui/apue/lib/error.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/error.c.o"
+  "/home/maorui/apue/lib/errorlog.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/errorlog.c.o"
+  "/home/maorui/apue/lib/lockreg.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/lockreg.c.o"
+  "/home/maorui/apue/lib/locktest.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/locktest.c.o"
+  "/home/maorui/apue/lib/nspipe.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/nspipe.c.o"
+  "/home/maorui/apue/lib/openmax.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/openmax.c.o"
+  "/home/maorui/apue/lib/pathalloc.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/pathalloc.c.o"
+  "/home/maorui/apue/lib/popen.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/popen.c.o"
+  "/home/maorui/apue/lib/prexit.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/prexit.c.o"
+  "/home/maorui/apue/lib/prmask.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/prmask.c.o"
+  "/home/maorui/apue/lib/ptyfork.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/ptyfork.c.o"
+  "/home/maorui/apue/lib/ptyopen.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/ptyopen.c.o"
+  "/home/maorui/apue/lib/readn.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/readn.c.o"
+  "/home/maorui/apue/lib/recvfd.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/recvfd.c.o"
+  "/home/maorui/apue/lib/semaph.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/semaph.c.o"
+  "/home/maorui/apue/lib/senderr.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/senderr.c.o"
+  "/home/maorui/apue/lib/sendfd.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/sendfd.c.o"
+  "/home/maorui/apue/lib/servaccept.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/servaccept.c.o"
+  "/home/maorui/apue/lib/servlisten.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/servlisten.c.o"
+  "/home/maorui/apue/lib/setfd.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/setfd.c.o"
+  "/home/maorui/apue/lib/setfl.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/setfl.c.o"
+  "/home/maorui/apue/lib/signal.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/signal.c.o"
+  "/home/maorui/apue/lib/signalintr.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/signalintr.c.o"
+  "/home/maorui/apue/lib/sleep.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/sleep.c.o"
+  "/home/maorui/apue/lib/sleepus.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/sleepus.c.o"
+  "/home/maorui/apue/lib/spipe.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/spipe.c.o"
+  "/home/maorui/apue/lib/strerror.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/strerror.c.o"
+  "/home/maorui/apue/lib/tellwait.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/tellwait.c.o"
+  "/home/maorui/apue/lib/ttymodes.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/ttymodes.c.o"
+  "/home/maorui/apue/lib/writen.c" "/home/maorui/apue/build/lib/CMakeFiles/apue.dir/writen.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
